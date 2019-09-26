eacc3be041117e59cf73502656f67c26065f8656
Fionalan727 <lanyufei727@gmail.com>
2019-09-25 20:13:41 -0400

    add sign in route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from "./pages/shop/shop.component"
-import Header from './components/header/header.component';
+
 import './App.scss';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from "./pages/shop/shop.component"
+import SignInAndSignUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component';
+import Header from './components/header/header.component';
 
 
 function App(){
@@ -14,6 +16,7 @@ function App(){
       <Switch>
         <Route exact path ='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
         
       </div>
---
+++ src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import'./sign-in-and-sing-up.styles.scss';
+
+const SignINAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+     SIGN IN
+    </div>
+);
+
+export default SignINAndSignUpPage;
