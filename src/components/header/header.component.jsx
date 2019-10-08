53818b3eb7ae51d8784cab05eef195c5bf8debed
Fionalan727 <lanyufei727@gmail.com>
2019-10-07 21:36:19 -0400

    update CartDropDown component in header component to toggle base on click

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,8 @@ import {connect} from 'react-redux';
 import {auth} from "../../firebase/firebase.utils";
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-const Header = ({currentUser}) => (
+
+const Header = ({currentUser,hidden}) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo"/>
@@ -27,12 +28,16 @@ const Header = ({currentUser}) => (
             }
             <CartIcon/>
         </div>
-        <CartDropdown/>
+        {
+            hidden ? null :
+            <CartDropdown/>
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser:state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
