08d78331898e652762fcc0defac4d093df39aaea
Fionalan727 <lanyufei727@gmail.com>
2019-10-25 20:49:06 -0400

    add onclick function to go to checkout page button

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,7 +7,7 @@ import {withRouter} from 'react-router-dom';
 import {selectCartItems} from '../../redux/cart/cart.selectors'
 import'./cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems, history}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -19,7 +19,7 @@ const CartDropdown = ({cartItems}) => (
             <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick = {() => history.push('./checkout')}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
