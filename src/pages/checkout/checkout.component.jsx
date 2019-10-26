c5f4e6f6d7b0de6f72c75b98b80aa2959e943883
Fionalan727 <lanyufei727@gmail.com>
2019-10-25 21:19:12 -0400

    add total to checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 import'./checkout.styles.scss';
 
-const CheckoutPage = () => (
+const CheckoutPage = ({cartItems,total}) => (
     <div className="checkout-page">
         <div className="checkout-header">
             <div className="header-block">
@@ -24,11 +24,19 @@ const CheckoutPage = () => (
                 <span>Remove</span>
             </div>
         </div>
-        
+        {
+            cartItems.map(cartItem => 
+                cartItem.name
+            )
+        }
+        <div className="total">
+            <span>TOTAL: ${total}</span>
+        </div>
     </div>
 )
 
 const mapStateToProps = createStructuredSelector({
-    cartItems : selectCartItems
+    cartItems : selectCartItems,
+    total: selectCartTotal
 });
-export default CheckoutPage;
+export default connect(mapStateToProps)(CheckoutPage);
