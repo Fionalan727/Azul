8af7dfdf6c941c2e5c34c1c9c5a67c69f4d1be9b
Fionalan727 <lanyufei727@gmail.com>
2019-11-01 03:45:40 -0400

    create category page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
-
+import CategoryPage from '../category/category.component'
 const ShopPage = ({match}) =>(
 
     <div className='shop-page'>
         <Route exact path = {`${match.path}`} component={CollectionsOverview}/>
+        <Route  path={`${match.path}/:categoryId`} component={CategoryPage}/>
     </div>
 )
 
