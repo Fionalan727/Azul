53fc311e702a757e94b9f07dcd611dffdd85fcd7
Fionalan727 <lanyufei727@gmail.com>
2019-11-01 03:28:39 -0400

    add stylse to collection-overview

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import './collection.styles.scss';
+import './collection-overview.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 import {selectCollections} from '../../redux/shop/shop.selectors'
 
