458d70136a737dc67e1e69536e70c0533515c68d
Fionalan727 <lanyufei727@gmail.com>
2019-11-01 03:20:00 -0400

    modify collection-overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import {selectCollections} from '../../redux/shop/shop.selectors'
 
@@ -13,7 +11,5 @@ const ShopPage = ({collections}) =>(
     </div>
 )
 
-const mapStateToProps  = createStructuredSelector({
-    collections: selectCollections
-})
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
