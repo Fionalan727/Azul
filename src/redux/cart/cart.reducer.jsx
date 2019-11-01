e240b22e600894a11ed476139e91c6ef9aee8b1c
Fionalan727 <lanyufei727@gmail.com>
2019-11-01 00:17:38 -0400

    add redux persist

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -22,7 +22,6 @@ const cartReducer = (state=INITIAL_STATE , action) => {
             return{
                 ...state,
                 cartItems:removeItemFromCart(state.cartItems,action.payload)
-                
             }
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return{
