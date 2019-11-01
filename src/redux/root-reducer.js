9a031b48b75728f8e3cfd5354710290d5dd4c354
Fionalan727 <lanyufei727@gmail.com>
2019-11-01 00:23:24 -0400

    modify store.js

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from "redux";
+import{persisReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 export default combineReducers({
