f9f7df789fc8cc999f4d45235db3f077bc7cc0a4
Fionalan727 <lanyufei727@gmail.com>
2019-11-05 23:42:39 -0500

    build custom stripecheckout

--- src/components/stripe-button/strip-button.component.jsx
+++ src/components/stripe-button/strip-button.component.jsx
@@ -3,9 +3,27 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({price}) => {
-    const priceFOrStripe = price * 100;
+    const priceForStripe = price * 100;
     const publishableKey = 'pk_test_xJkm9uMmun4G58dEEFxlC36W001rRdNhNl'
-
     
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful!')
+    }
+    return(
+        <StripeCheckout
+        label = 'Pay Now'
+        name='CRWN Clothing Ltd.'
+        billingAddress
+        shippingAddress
+        image='https://svgshare.com/i/CUz.svg'
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+        />
+    )
 
+}
 export default StripeCheckoutButton;
